b1cea6fd884a83b0d56976e369ee85a4af4c4973
roelvink <floortje.roelvink@deltares.nl>
2023-12-14 10:45:06 +0100

    set run_models to True :)

--- src/cosmos/cosmos_configuration.py
+++ src/cosmos/cosmos_configuration.py
@@ -89,8 +89,8 @@ class Cycle:
         self.run_mode        = "serial"
         self.only_run_ensemble = False
         self.just_initialize = False
-        self.run_models = False
-        
+        self.run_models      = True
+
 class Configuration:
     """CoSMoS Configuration class.
 
