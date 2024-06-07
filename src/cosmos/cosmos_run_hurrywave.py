298b2901dcd5acabc213d005332d6c627dca8309
roeldegoede <roeldegoede@outlook.com>
2024-06-07 10:07:51 +0200

    correct little mistake in order of wave-maps

--- src/cosmos/cosmos_run_hurrywave.py
+++ src/cosmos/cosmos_run_hurrywave.py
@@ -194,9 +194,9 @@ def map_tiles(config):
             color_values = config["hm0_map"]["color_map"]["contours"]
 
             pathstr = []
-            pathstr.append("combined_" + (t0).strftime("%Y%m%d_%HZ") + "_" + (t1).strftime("%Y%m%d_%HZ"))
             for it, t in enumerate(requested_times):
                 pathstr.append((t - dt).strftime("%Y%m%d_%HZ") + "_" + (t).strftime("%Y%m%d_%HZ"))
+            pathstr.append("combined_" + (t0).strftime("%Y%m%d_%HZ") + "_" + (t1).strftime("%Y%m%d_%HZ"))
 
             hm0max_file = "./hurrywave_map.nc"
             if config["ensemble"]:
