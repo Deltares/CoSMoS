648d85192c2fee0b5c317b963c9d10fa3d4c1ab3
kvanasselt <koenvanasselt@live.nl>
2024-06-12 12:13:09 +0200

    keyword_error

--- src/cosmos/cosmos_run_parallel.py
+++ src/cosmos/cosmos_run_parallel.py
@@ -97,7 +97,7 @@ class CosmosRunParallel:
                     
                     # Copy remote folder to local copy
                     try:
-                        shutil.copytree(os.path.join(model_path), os.path.join(self.local_path, model_name), exist_ok = True)
+                        shutil.copytree(os.path.join(model_path), os.path.join(self.local_path, model_name), dirs_exist_ok = True)
                     except Exception as e: 
                         os.mkdir(os.path.join(self.local_path, model_name))
                         print("Could not find model on p-drive,  {}".format(str(e)))
@@ -106,7 +106,7 @@ class CosmosRunParallel:
                     if not os.path.exists(os.path.join(self.local_path, model_name)):
                         time.sleep(5)
                         try:
-                            shutil.copytree(os.path.join(model_path), os.path.join(self.local_path, model_name), exist_ok = True)
+                            shutil.copytree(os.path.join(model_path), os.path.join(self.local_path, model_name), dirs_exist_ok = True)
                         except Exception as e:
                             print("Retry to copy files failed, sending .txt file to jobs folder op p-drive and removing on locally.")
                             file_name = os.path.join(job_path)
@@ -120,7 +120,7 @@ class CosmosRunParallel:
                     if len(files_local) == 0:
                         time.sleep(5)
                         try:
-                            shutil.copytree(os.path.join(model_path), os.path.join(self.local_path, model_name), exist_ok = True)
+                            shutil.copytree(os.path.join(model_path), os.path.join(self.local_path, model_name), dirs_exist_ok = True)
                         except Exception as e:
                             print("Retry to copy files failed, sending .txt file to jobs folder op p-drive and removing locally.")
                             file_name = os.path.join(job_path)
